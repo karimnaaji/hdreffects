48899446922bf51833f7927678b4461111204908
Karim Naaji <karim.naaji@gmail.com>
2013-11-24 12:23:29 -0500

    rendering to texture

---
+++ build/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+make
+if [[ $? == 0 ]] 
+then
+    date >> out.log 
+    if [[ -z $1 ]] 
+    then
+       ./hdr.out 2>> out.log
+    else 
+       ./hdr.out -w $1 -h $2 -f $3 2>> out.log
+    fi
+
+    #print todos
+    for f in `find .. -name "*.cpp"`; do
+        out=`cat $f | grep "TODO*"`
+        if [[ $? == 0 ]]
+        then
+            echo "todo in $f :"
+            echo "$out"
+        fi
+    done
+fi
